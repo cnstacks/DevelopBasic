c1282f2d361ae7f3e749ee2f174d4590b6cdfbd3
tqtl911@163.com <19930911cXS>
2018-10-08 19:53:23 +0800

    Python中的deadloop，死循环！

---
+++ Models-01/1-27 DeadLoop死循环.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Project: DevelopBasic 
+# Software: PyCharm
+# DateTime    : 2018-10-08 19:50
+# File    : 1-27 DeadLoop死循环.py
+# __author__: 天晴天朗
+# Email   : tqtl@tqtl.org
+
+
+count = 0
+while True:
+    print("你是风儿我是沙，缠缠绵绵到天涯！", count)
+    count += 1
