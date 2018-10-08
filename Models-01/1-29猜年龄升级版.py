a675cab728b5e683c2d7107aa679e6d62611680f
tqtl911@163.com <19930911cXS>
2018-10-08 20:11:40 +0800

    循环猜测用户的年龄！

---
+++ Models-01/1-29猜年龄升级版.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Project: DevelopBasic 
+# Software: PyCharm
+# DateTime    : 2018-10-08 20:01
+# File    : 1-29猜年龄升级版.py
+# __author__: 天晴天朗
+# Email   : tqtl@tqtl.org
+
+
+times = 0
+age = 26  # 放在外侧，防止每次循环调用！
+while times < 3:
+    guess_age = int(input("GuessAge:"))
+    if guess_age > age:
+        print("Try Smaller")
+    elif guess_age < age:
+        print("Try Bigger")
+    else:
+        print("恭喜你，猜对啦！")
+        break
+    times += 1
+    if times == 3:
+        choice = input("没猜对，你还想继续吗？（Y|y）")
+        if choice == "y" or choice == "Y":
+            times = 0
+else:
+    print("您已经猜测了%d次,次数已经用完，游戏结束啦！" % times)
