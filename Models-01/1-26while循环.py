5b7ec27eed188cf27a56adef37cf1bfc1e941ab0
tqtl911@163.com <19930911cXS>
2018-10-08 19:50:16 +0800

    打印分数的小程序

---
+++ Models-01/1-26while循环.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Project: DevelopBasic 
+# Software: PyCharm
+# DateTime    : 2018-10-08 19:22
+# File    : 1-26while循环.py
+# __author__: 天晴天朗
+# Email   : tqtl@tqtl.org
+
+num = 1
+while num <= 100:
+    if num == 50:
+        pass
+    elif num >= 60 and num <= 80:
+        print("num*num:",num * num)
+    else:
+        print("Loop:", num)
+    num += 1
